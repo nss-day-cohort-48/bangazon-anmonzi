4421585a28431015d60a5785e335c9c77d8f9a2b
Ashton Monzi <anmonzi@gmail.com>
2021-09-01 16:01:19 -0500

    prepped sql query for next report

--- test.sql
+++ test.sql
@@ -16,3 +16,25 @@ FROM
 bangazonapi_product AS bp
 WHERE bp.price <= 999.00
 ORDER BY bp.price DESC;
+
+
+SELECT 
+    bo.id AS order_id,
+    user.first_name || ' ' || user.last_name AS full_name,
+    bp.merchant_name AS payment_type,
+    SUM(product.price) AS total_price
+FROM 
+    bangazonapi_order AS bo
+JOIN
+    bangazonapi_customer AS bc
+ON bo.customer_id = bc.id
+JOIN auth_user AS user
+ON bc.user_id = user.id
+JOIN bangazonapi_payment AS bp
+ON bo.payment_type_id = bp.id
+JOIN bangazonapi_orderproduct AS bop
+ON bo.id = bop.order_id
+JOIN bangazonapi_product AS product
+ON bop.product_id = product.id
+WHERE bo.payment_type_id IS NOT NULL
+GROUP BY bo.id;
