57885bb5b5162ab3a122c921457c4c28e8aa1ec3
Ashton Monzi <anmonzi@gmail.com>
2021-09-01 15:25:28 -0500

    inexpensive product report complete

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import expensive_products
+from .views import expensive_products, inexpensive_products
 
 urlpatterns = [
     path('reports/expensiveproducts', expensive_products),
+    path('reports/inexpensiveproducts', inexpensive_products),
 ]
--- test.sql
+++ test.sql
@@ -7,3 +7,12 @@ WHERE bp.price >= 1000.00
 ORDER BY bp.price ASC;
 
 
+SELECT 
+bp.price,
+bp.name,
+bp.description,
+bp.id
+FROM 
+bangazonapi_product AS bp
+WHERE bp.price <= 999.00
+ORDER BY bp.price DESC;
