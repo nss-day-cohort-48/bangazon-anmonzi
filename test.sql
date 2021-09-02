528b9cb594452e38e17646f46cc55141c1805c3b
Ashton Monzi <anmonzi@gmail.com>
2021-09-02 09:27:56 -0500

    setup sql queries for tickets 3-4

--- test.sql
+++ test.sql
@@ -27,14 +27,40 @@ FROM
     bangazonapi_order AS bo
 JOIN
     bangazonapi_customer AS bc
-ON bo.customer_id = bc.id
-JOIN auth_user AS user
-ON bc.user_id = user.id
-JOIN bangazonapi_payment AS bp
-ON bo.payment_type_id = bp.id
-JOIN bangazonapi_orderproduct AS bop
-ON bo.id = bop.order_id
-JOIN bangazonapi_product AS product
-ON bop.product_id = product.id
+    ON bo.customer_id = bc.id
+JOIN 
+    auth_user AS user
+    ON bc.user_id = user.id
+JOIN 
+    bangazonapi_payment AS bp
+    ON bo.payment_type_id = bp.id
+JOIN 
+    bangazonapi_orderproduct AS bop
+    ON bo.id = bop.order_id
+JOIN 
+    bangazonapi_product AS product
+    ON bop.product_id = product.id
 WHERE bo.payment_type_id IS NOT NULL
 GROUP BY bo.id;
+
+
+
+SELECT
+    bo.id AS order_id,
+    user.first_name || ' ' || user.last_name AS full_name,
+    sum(bp.price) AS order_total,
+    count(bop.id) AS num_items
+FROM 
+    bangazonapi_order AS bo
+JOIN 
+    bangazonapi_customer AS bc 
+    ON bc.id = bo.customer_id
+JOIN 
+    auth_user AS user 
+    ON user.id = bc.user_id
+JOIN bangazonapi_orderproduct AS bop 
+    ON bop.order_id = bo.id
+JOIN bangazonapi_product AS bp 
+    ON bop.product_id = bp.id
+WHERE bo.payment_type_id IS NULL
+GROUP BY order_id;
