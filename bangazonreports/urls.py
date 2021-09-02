46d93dad7a996fd966a0d10d4c4ca2429cd79178
Ashton Monzi <anmonzi@gmail.com>
2021-09-02 11:28:07 -0500

    favorited seller report done

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import expensive_products, inexpensive_products
+from .views import expensive_products, inexpensive_products, favorited_sellers_by_customer_list
 
 urlpatterns = [
     path('reports/expensiveproducts', expensive_products),
     path('reports/inexpensiveproducts', inexpensive_products),
+
+
+    path('reports/favoritesbycustomers', favorited_sellers_by_customer_list),
 ]
