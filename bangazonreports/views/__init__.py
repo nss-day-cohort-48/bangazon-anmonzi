3f5786a70202da1d61825fc52cc1f182311d229b
Ashton Monzi <anmonzi@gmail.com>
2021-09-02 10:03:43 -0500

    incomplete order report done

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .users.expensiveproducts import expensive_products
 from .users.inexpensiveproducts import inexpensive_products
 from .users.completeorders import complete_orders
+from .users.incompleteorders import incomplete_orders
